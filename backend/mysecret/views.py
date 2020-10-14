5ab9432caac54be8091dd2b78feae5f62523365c
dikos1337 <59239573+dikos1337@users.noreply.github.com>
2020-10-14 03:14:50 +0300

    Убрал неиспользуемый view

--- backend/mysecret/views.py
+++ backend/mysecret/views.py
@@ -1,6 +1,4 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 
 
 # Create your views here.
-def index_page(request):
-    return render(request, 'mysecret/index.html', {})
