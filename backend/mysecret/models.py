1244659d08cf8e6d50847697fdb8681f0dd355f6
dikos1337 <59239573+dikos1337@users.noreply.github.com>
2020-10-20 02:49:51 +0300

    Поправил урлы, поправил моедель

--- backend/mysecret/models.py
+++ backend/mysecret/models.py
@@ -3,11 +3,15 @@ from django.db import models
 
 # Create your models here.
 class Secret(models.Model):
-    secret = models.TextField(max_length=8192)
-    passphrase = models.CharField(max_length=64)
-    lifetime = models.FloatField()
-    created_date = models.DateTimeField("Дата создания")
-    viewed = models.BooleanField(default=False)  # default False
+    created_date = models.DateTimeField(verbose_name="Дата создания",
+                                        auto_now=True)
+    secret = models.TextField(verbose_name="Содержание секрета",
+                              max_length=8192)
+    passphrase = models.CharField(verbose_name="Фраза-пропуск", max_length=64)
+    lifetime = models.FloatField(
+        verbose_name="Lifetime, время жизни секрета", )
+    viewed = models.BooleanField(verbose_name="Просмотрел ли секрет",
+                                 default=False)  # default False
 
     def __str__(self):
-        return self.secret[:100] + ('...' if len(self.secret) > 100 else '')
+        return self.secret[:100] + ("..." if len(self.secret) > 100 else "")
--- backend/mysecret/urls.py
+++ backend/mysecret/urls.py
@@ -1,9 +1,7 @@
-from django.urls import path
+# from django.urls import path
 
 # from . import views
-from django.views.generic import TemplateView
-
 
 urlpatterns = [
-    path("", TemplateView.as_view(template_name="mysecret/index.html")),
+    # path("", TemplateView.as_view(template_name="mysecret/index.html")),
 ]
