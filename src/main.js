eea446ff9731237620601afb29132bda1b0164ad
dikos1337 <59239573+dikos1337@users.noreply.github.com>
2020-10-24 04:28:10 +0300

    Поправил баг, отправляю csrf token вместе с формой

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import Vue from "vue";
 import Vuelidate from "vuelidate";
 import App from "./App.vue";
+import axios from 'axios'
+
+axios.defaults.xsrfHeaderName = "X-CSRFTOKEN"
+axios.defaults.xsrfCookieName = "csrftoken"
 
 Vue.config.productionTip = false;
-
 Vue.use(Vuelidate)
 
 new Vue({
